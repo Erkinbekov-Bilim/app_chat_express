29cac57fb1585daa78c3be26469088f4713800b5
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-24 16:04:31 +0600

    feat(frontend): add getMessages async thunk

--- frontend/src/features/chat/chat.api.ts
+++ frontend/src/features/chat/chat.api.ts
@@ -1,10 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { IMessageMutation } from '../../types/chat/messageMutation.type';
 import { axiosApi } from '../../api/axiosApi';
+import type { IMessage } from '../../types/chat/message.type';
 
 export const postNewMessage = createAsyncThunk<void, IMessageMutation>(
   'chat/postNewMessage',
   async (message) => {
     await axiosApi.post<IMessageMutation>('messages', message);
+  },
+);
+
+export const getMessages = createAsyncThunk<IMessage[], void>(
+  'chat/getMessages',
+  async () => {
+    const { data } = await axiosApi.get<IMessage[]>('messages');
+    return data;
   },
 );
