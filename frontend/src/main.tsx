a88efb7035a0b97f75d66cbef097838818304225
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-24 15:06:05 +0600

    feat: connect redux store to application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import CssBaseline from '@mui/material/CssBaseline';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
